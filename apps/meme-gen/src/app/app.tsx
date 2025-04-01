79ca8091a80ddb45cfb586302eeb8f5973da8461
Michael Uzan <looply3@gmail.com>
2025-04-01 23:14:28 +0300

    add chakra ui

--- apps/meme-gen/src/app/app.tsx
+++ apps/meme-gen/src/app/app.tsx
@@ -1,42 +1,10 @@
-import { Route, Routes, Link } from 'react-router-dom';
+import { Box} from '@chakra-ui/react';
 
 export function App() {
   return (
-    <div>
-      <br />
-      <hr />
-      <br />
-      <div role="navigation">
-        <ul>
-          <li>
-            <Link to="/">Home</Link>
-          </li>
-          <li>
-            <Link to="/page-2">Page 2</Link>
-          </li>
-        </ul>
-      </div>
-      <Routes>
-        <Route
-          path="/"
-          element={
-            <div>
-              Hello Wrold!
-              <Link to="/page-2">Click here for page 2.</Link>
-            </div>
-          }
-        />
-        <Route
-          path="/page-2"
-          element={
-            <div>
-              Bye world!
-              <Link to="/">Click here for page 1.</Link>
-            </div>
-          }
-        />
-      </Routes>
-    </div>
+<Box>
+Hello World!!
+</Box>
   );
 }
 
--- apps/meme-gen/src/main.tsx
+++ apps/meme-gen/src/main.tsx
@@ -2,6 +2,8 @@ import { StrictMode } from 'react';
 import { BrowserRouter } from 'react-router-dom';
 import * as ReactDOM from 'react-dom/client';
 import App from './app/app';
+import { ChakraProvider } from '@chakra-ui/react';
+import { system } from './chakra-system/chakra-system';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
@@ -10,7 +12,9 @@ const root = ReactDOM.createRoot(
 root.render(
   <StrictMode>
     <BrowserRouter>
+    <ChakraProvider value={system} >
       <App />
+    </ChakraProvider>
     </BrowserRouter>
   </StrictMode>
 );
